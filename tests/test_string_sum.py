90c4e8b40d02094b1ee1dc372c8370fcf2f89386
Mateo Lamthi <mateo.lamthi@outlook.com>
2024-10-14 11:31:26 +0200

    Add tests for large number handling

--- tests/test_string_sum.py
+++ tests/test_string_sum.py
@@ -13,5 +13,8 @@ class TestStringSum(unittest.TestCase):
         self.assertEqual(stringSum("2", ""), "2")
         self.assertEqual(stringSum("", ""), "0")
 
+    def test_large_numbers(self):
+        self.assertEqual(stringSum("12345678901234567890", "98765432109876543210"), "111111111011111111100")
+
 if __name__ == '__main__':
     unittest.main()
